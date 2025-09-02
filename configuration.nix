91dfef91baa4a74f237ffd3d56d98f4ce09daf16
Evgeniy <suetologai@gmail.com>
2025-09-02 22:12:53 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [ ./hardware-configuration.nix ];
 
-  # --- BOOTLOADER (systemd-boot for UEFI) ---
+  # --- BOOTLOADER ---
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
@@ -19,27 +19,22 @@
   i18n.defaultLocale = "en_US.UTF-8";
   
   console.keyMap = "us";
-  
-  # NEW: Modern, global keyboard configuration for both SDDM and Wayland session.
   services.keyboard = {
     layout = "us,ru";
     options = [ "grp:win_space_toggle" ];
   };
   
-  # --- GRAPHICAL DESKTOP ENVIRONMENT (Minimal KDE Plasma on Wayland) ---
+  # --- GRAPHICS (KDE + Wayland) ---
   services.xserver.enable = true;
-  
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
-  # NEW: Force default session to Wayland.
   services.xserver.displayManager.sddm.defaultSession = "plasmawayland";
 
   services.xserver.desktopManager.plasma5.enable = true;
 
-  services.xserver.videoDrivers = [ "modesetting" ];
+  services.xserver.videoDrivers = [ "modesetting" "intel" "nvidia" ];
 
-  # --- SOUND SYSTEM (PipeWire) ---
-  # sound.enable = true; was removed as it's deprecated.
+  # --- SOUND ---
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -49,7 +44,7 @@
     pulse.enable = true;
   };
 
-  # --- USER ACCOUNTS ---
+  # --- USERS ---
   users.users.Sueta = {
     isNormalUser = true;
     description = "Sueta";
@@ -58,33 +53,43 @@
   };
   security.sudo.wheelNeedsPassword = true;
 
-  # --- SYSTEM PACKAGES ---
+  # --- PACKAGES ---
   environment.systemPackages = with pkgs; [
+    # Базовые
     git
     wget
     firefox
+    discord
     kdePackages.konsole
     kdePackages.dolphin
     kdePackages.kate
     kdePackages.spectacle
+
+    # Wi-Fi и сетевой анализ
+    aircrack-ng
+    wireshark
+    kismet
+    nmap
+
+    # Хакинг / пентест
+    metasploit
+    hydra
   ];
 
   # --- HARDWARE & SERVICES ---
-  hardware.bluetooth.enable = false;
+  hardware.bluetooth.enable = true;
   services.printing.enable = true;
 
+  # --- WAYLAND PORTALS ---
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
+    extraPortals = with pkgs; [ xdg-desktop-portal-kde ];
   };
 
-  # --- NIX PACKAGE MANAGER SETTINGS ---
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
+  # --- POWER MANAGEMENT ---
+  services.tlp.enable = true;
+}
+
   
   system.stateVersion = "25.05";
 }
